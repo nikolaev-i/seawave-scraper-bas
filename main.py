a4630d91a434d869591abc310ee90b85d737cf57
ivan <eng.ivan.nikolaev@gmail.com>
2023-07-26 14:02:04 +0300

    sanitizing scraped data

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
-from vars import PATH, site
-import bs4 as bs 
+from vars import PATH, site, page_source
+from bs4 import BeautifulSoup as bs
+from datetime import datetime, tzinfo
+from dateutil import tz
+import pytz
+import re
 
 
 
@@ -15,8 +20,11 @@ options.add_experimental_option("detach", True)
 
 service = Service(PATH)
 
+"""
+
 driver = webdriver.Chrome(service=service,
                           options=options)
+
 
 
 driver.get(site)
@@ -24,6 +32,35 @@ driver.get(site)
 dropdownbox = driver.find_elements(by=By.TAG_NAME, value="Option")
 dropdownbox[1].click()                                   
 
-
+page_source = driver.page_source
 
 driver.close()
+"""
+
+
+
+source = bs(page_source,"html.parser")
+table_header = source.find_all('tbody')[2]
+data_table = table_header.find_all('td')
+
+data_list = [] 
+single_record = []
+
+for data in data_table:
+  values = data.text.strip('\xa0')
+  data_list.append(values)
+
+
+for data in data_list:
+  if len(single_record) != 8:
+    single_record.append(data)
+
+single_record.extend(single_record[7].split(' '))
+single_record.pop(7)
+print(single_record)
+
+
+
+
+
+
